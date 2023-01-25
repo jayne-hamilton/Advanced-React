629f3bf1b5f15df63a4a18dfc5fe38042cd9e4eb
Jayne Hamilton <jayneehamilton@gmail.com>
2023-01-25 21:36:54 +1300

    Nav added

---
+++ sick-fits/frontend/components/Nav.js
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+export default function Nav() {
+    return (
+        <nav>
+            <Link href="/">products</Link>
+            <Link href="/">sell</Link>
+            <Link href="/">orders</Link>
+            <Link href="/">accounts</Link>
+        </nav>
+    )
+
+}
